b4db1615324b665a234feeae73e716ab41d1d25d
axel.legue <axel.legue31@gmail.com>
2020-04-22 01:19:11 +0200

    feat: play sound from local assets 	- add flutter package audio players 	- add icon button that play sound when clicked

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  static AudioCache player = AudioCache();
+
+  playSound(String url) {
+    player.play(url);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: IconButton(
+              icon: Icon(
+                Icons.volume_up,
+                color: Colors.pink,
+              ),
+              tooltip: 'Increase volume by 10',
+              onPressed: () {
+                playSound('note1.wav');
+              },
+            ),
+          ),
         ),
       ),
     );
